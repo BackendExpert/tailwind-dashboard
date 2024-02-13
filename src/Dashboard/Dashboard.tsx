c38abfbe29220d3c7a5faa9a55e8daf3895edcaf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-13 18:21:25 +0530

    Uodate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -2,6 +2,9 @@ const Dashboard = () => {
   return (
     <div className="flex">
         <div className={`w-72 h-screen bg-gray-900`}>SideBar</div>
+        <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+            <h1>Home Page</h1>
+        </div>
     </div>
   )
 }
