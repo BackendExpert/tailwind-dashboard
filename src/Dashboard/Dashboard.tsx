28ec55cb6bbf7bd060e5855eae60d50b678f9998
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-13 19:41:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -3,7 +3,13 @@ import SideBar from "../Sidebar/SideBar"
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="flex">
+        <SideBar />
+
+        <div className="">
+            <h1 className="font-semibold">Dashboard</h1>
+        </div>
+    </div>
   )
 }
 
