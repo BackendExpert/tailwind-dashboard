0776808ef6ba3b1cde0a8f0c0cddc59170d29d39
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-13 18:37:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import IonIcon from "@reacticons/ionicons"
+import React from "react"
 
 const Dashboard = () => {
   return (
@@ -6,6 +7,11 @@ const Dashboard = () => {
         <div className="bg-gray-800 min-h-screen w-72 text-white px-4">
             <div className="py-3 flex justify-end">
                 <IonIcon name="menu" size="large"></IonIcon>
+            </div>
+            <div className="mt-4 flex lflex-col pag-4 relative">
+                <div>
+                    Dashboard
+                </div>
             </div>
         </div>
         <div className="p-7 text-2xl font-semibold flex-1 h-screen">
