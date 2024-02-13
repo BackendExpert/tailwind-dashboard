477e47b0593e46dacefb1dc249b5507be6754c16
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-13 18:16:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Dashboard" element={}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
